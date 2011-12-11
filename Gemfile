ef010836ab92292674b97b6f12394c41cc896abc
javierburon@colaborativa.eu <javito@Pikachete.local>
2011-12-11 17:37:37 +0100

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -8,4 +8,5 @@ end
 group :test do
   gem 'rspec', '2.0.1' 
   gem 'webrat', '0.7.1'
+  gem 'spork'
 end
--- config/application.rb
+++ config/application.rb
@@ -49,6 +49,11 @@ module SampleApp
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.0
+    '
+      ### Part of a Spork hack. See http://bit.ly/arY19y if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+      # Work around initializer in railties/lib/rails/application/bootstrap.rb ActiveSupport::Dependencies.mechanism = :load
+      end 
   end
 end
